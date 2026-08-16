200dd1d7855d1ef6d2d8ff029814081fc5631a6b
Felipe Marins <fmarins.sgs@gmail.com>
2026-08-16 18:26:11 -0300

    Simplifica classe Rect e adiciona método de mudança de tamnho para Figure

--- Projeto/figures/Figure.java
+++ Projeto/figures/Figure.java
@@ -3,8 +3,31 @@ package figures;
 import java.awt.*;
 
 public abstract class Figure {
-	int x, y;
-	int w, h;
+    public int x, y;
+    public int w, h;
 
-	public abstract void paint (Graphics g);
+    public Figure(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.w = width;
+        this.h = height;
+    }
+
+    public abstract void paint(Graphics g);
+
+    public void setSizeRelativeTo(int width, int height, int originX, int originY) {
+        if (width < 0) {
+            this.x = originX + width;
+        } else {
+            this.x = originX;
+        }
+        this.w = Math.abs(width);
+
+        if (height < 0) {
+            this.y = originY + height;
+        } else {
+            this.y = originY;
+        }
+        this.h = Math.abs(height);
+    }
 }
--- Projeto/figures/Rect.java
+++ Projeto/figures/Rect.java
@@ -3,29 +3,24 @@ package figures;
 import java.awt.*;
 
 public class Rect extends Figure {
-    int x, y;
-    int w, h;
     Color outlineColor, backgroundColor;
 
-    public Rect (int x, int y, int w, int h, Color outlineColor, Color backgroundColor) {
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-	this.outlineColor = outlineColor;
-	this.backgroundColor = backgroundColor;
+    public Rect(int x, int y, int width, int height, Color outlineColor, Color backgroundColor) {
+        super(x, y, width, height);
+        this.outlineColor = outlineColor;
+        this.backgroundColor = backgroundColor;
     }
 
-    public void print () {
+    public void print() {
         System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
-            this.w, this.h, this.x, this.y);
+                this.w, this.h, this.x, this.y);
     }
 
-    public void paint (Graphics g) {
+    public void paint(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-	g.setColor(backgroundColor);
-	g2d.fillRect(this.x,this.y, this.w,this.h);
-	g.setColor(outlineColor);
-        g2d.drawRect(this.x,this.y, this.w,this.h);
+        g.setColor(backgroundColor);
+        g2d.fillRect(this.x, this.y, this.w, this.h);
+        g.setColor(outlineColor);
+        g2d.drawRect(this.x, this.y, this.w, this.h);
     }
 }
